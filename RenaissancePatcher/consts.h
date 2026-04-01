0b6c336b9982f8658194367e4e9a6b6b0582bb2d
n0cha3 <62072039+n0cha3@users.noreply.github.com>
2026-04-02 02:20:11 +0300

    Special thanks

--- RenaissancePatcher/consts.h
+++ RenaissancePatcher/consts.h
@@ -5,7 +5,7 @@
 #define RENAISIMPORT (PSTR)"MainHakVzlom"
 #define IMPDLL (PSTR)"RenaissancePatch.dll"
 #define IMPSEC (PSTR)".rnaisan"
-#define EASTER_EGG (PSTR)"Renaissance Team Veselcraft motionarium chelka0 Eversiege Daniel Myslivets Neru n0cha3 MrComPort Aboba224 Milya Ash flydelick Akaremii"
+#define EASTER_EGG (PSTR)"Renaissance Team Veselcraft motionarium chelka0 Eversiege Daniel Myslivets Neru n0cha3 MrComPort Aboba224 Milya Ash flydelick, Testing: Akaremii"
 #define DEFAULT_SERVER (PWSTR)L"proto.mrim.su"
 #define DEFAULT_AVATAR_SERVER (PWSTR)L"obraz.mrim.su"
 #define DEFAULT_MRIM_SERVICES (PWSTR)L"obraz.mrim.su"
